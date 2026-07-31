13661243b8a27df85a07f818b7b18849f28371a0
0xRafay <abdulrafay.src@gmail.com>
2026-07-31 05:29:03 -0400

    feat: add public Scanner class

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -6,3 +6,13 @@ import java.util.List;
 import java.util.Map;
 
 import static com.craftinginterpreters.lox.TokenType.*;
+
+public class Scanner {
+
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    Scanner(String source){
+        this.source = source;
+    }
+}
