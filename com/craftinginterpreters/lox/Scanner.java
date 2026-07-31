2f4ddcb974c3255dec601addc2e797610949600a
0xRafay <abdulrafay.src@gmail.com>
2026-07-31 05:34:17 -0400

    feat: add scanTokens()

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -10,9 +10,25 @@ import static com.craftinginterpreters.lox.TokenType.*;
 public class Scanner {
 
     private final String source;
-    private final List<Token> tokens = new ArrayList<>();
 
-    Scanner(String source){
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
         this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanTokens();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+    private boolean isAtEnd() {
+        return current >= source.length();
     }
 }
