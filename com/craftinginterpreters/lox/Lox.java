a1931907a10c9b4aaba315b569018ef0befbd6ea
0xRafay <abdulrafay.src@gmail.com>
2026-07-31 05:09:03 -0400

    feat: add error condition

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 public class Lox {
   static boolean hadError = false;
+
   public static void main(String[] args) throws IOException {
     if (args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -25,6 +26,9 @@ public class Lox {
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
+    if (hadError) {
+      System.exit(65);
+    }
   }
 
   private static void runPrompt() throws IOException {
@@ -38,6 +42,7 @@ public class Lox {
         break;
       }
       run(line);
+      hadError = false;
     }
   }
 
@@ -54,7 +59,7 @@ public class Lox {
     report(line, "", message);
   }
 
-  private static void report(int line, String where, String message){
+  private static void report(int line, String where, String message) {
     System.err.println("[line " + line + "] Error" + where + ": " + message);
     hadError = true;
   }
