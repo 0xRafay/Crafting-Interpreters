c4a08c1a742572d0269b834336aa851b4e43c8e0
0xRafay <abdulrafay.src@gmail.com>
2026-07-31 04:48:40 -0400

    feat: add runPrompt, runFile, run()

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -1,12 +1,15 @@
 package com.craftinginterpreters.lox;
 
-import java.awt.List;
+import java.util.List;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
+
+import jdk.jfr.internal.query.Report;
 
 public class Lox {
   public static void main(String[] args) throws IOException {
@@ -17,6 +20,34 @@ public class Lox {
       runFile(args[0]);
     } else {
       runPrompt();
+    }
+  }
+
+  private static void runFile(String path) throws IOException {
+    byte[] bytes = Files.readAllBytes(Paths.get(path));
+    run(new String(bytes, Charset.defaultCharset()));
+  }
+
+  private static void runPrompt() throws IOException {
+    InputStreamReader input = new InputStreamReader(System.in);
+    BufferedReader reader = new BufferedReader(input);
+
+    for (;;) {
+      System.out.print("> ");
+      String line = reader.readLine();
+      if (line == null) {
+        break;
+      }
+      run(line);
+    }
+  }
+
+  private static void run(String source) {
+    Scanner scanner = new Scanner(source);
+    List<Token> tokens = scanner.scanTokens();
+
+    for (Token token : tokens) {
+      System.out.println(token);
     }
   }
 
