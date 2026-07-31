e17be4bcc232c58d81dfaa2f15217983d6ddca48
0xRafay <abdulrafay.src@gmail.com>
2026-07-31 05:24:15 -0400

    feat: define TokenType

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -9,6 +9,24 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 
+class Token {
+  final TokenType type;
+  final String lexeme;
+  final Object literal;
+  final int line;
+
+  Token(TokenType type, String lexeme, Object literal, int line) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  public String toString() {
+    return type + " " + lexeme + " " + literal;
+  }
+}
+
 public class Lox {
   static boolean hadError = false;
 
