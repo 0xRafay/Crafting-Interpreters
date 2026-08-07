7a91453b9a9128dfe99bfa363d5597c2987db4ce
0xRafay <abdulrafay.src@gmail.com>
2026-08-07 03:30:27 -0400

    chore(4-space indentation): ahem... so i changed my mind

--- com/craftinginterpreters/lox/Scanner.java
+++ com/craftinginterpreters/lox/Scanner.java
@@ -7,27 +7,27 @@ import java.util.List;
 
 public class Scanner {
 
-  private final String source;
+    private final String source;
 
-  private final List<Token> tokens = new ArrayList<>();
-  private int start = 0;
-  private int current = 0;
-  private int line = 1;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
 
-  Scanner(String source) {
-    this.source = source;
-  }
-
-  List<Token> scanTokens() {
-    while (!isAtEnd()) {
-      start = current;
-      scanTokens();
+    Scanner(String source) {
+        this.source = source;
     }
-    tokens.add(new Token(EOF, "", null, line));
-    return tokens;
-  }
 
-  private boolean isAtEnd() {
-    return current >= source.length();
-  }
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanTokens();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
 }
